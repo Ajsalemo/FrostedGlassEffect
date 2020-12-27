cd0adf9c1bbc574d77742bd7ff59cba156310001
Anthony Salemo <ajssalemo@gmail.com>
2020-12-27 15:14:50 -0500

    updated path aliases, changed background

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { LazyLoadImageModule } from 'ng-lazyload-image';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from '@components/home/app.component';
-
+import { GlassComponent } from '@components/glass/glass.component';
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, GlassComponent],
+  imports: [BrowserModule, AppRoutingModule, LazyLoadImageModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ client/tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = (isProd) => ({
+  prefix: "",
+  purge: {
+    enabled: isProd,
+    content: ["**/*.html", "**/*.ts"],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      boxShadow: {
+        radial: "inset 0 0 100px black",
+        glass: "0 0 1rem 0 rgba(0, 0, 0, .2)",
+      },
+      height: {
+        112: "26rem",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+});
