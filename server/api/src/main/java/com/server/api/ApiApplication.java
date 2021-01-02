2f201c7ca7f26043235b813bf32415bb0bf6997b
Anthony Salemo <ajssalemo@gmail.com>
2021-01-01 22:49:25 -0500

    updated the home controller

--- server/api/src/main/java/com/server/api/ApiApplication.java
+++ server/api/src/main/java/com/server/api/ApiApplication.java
@@ -2,12 +2,28 @@ package com.server.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ApiApplication {
-
+	public String description;	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String home() {
+		setDescription("Frosted Glass API");
+		return description;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
