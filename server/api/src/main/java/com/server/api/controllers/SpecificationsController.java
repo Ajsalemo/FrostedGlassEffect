3250e2a5d7c584c56be150c90c57af35a3b50d7b
Anthony Salemo <ajssalemo@gmail.com>
2021-01-03 17:17:00 -0500

    fixing pipeline var issues

--- server/api/src/main/java/com/server/api/controllers/SpecificationsController.java
+++ server/api/src/main/java/com/server/api/controllers/SpecificationsController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpecificationsController {
     @Autowired
-    public String specificationDescription;
     private SpecificationRepository specificationRepository;
     
     @GetMapping("/specifications")
