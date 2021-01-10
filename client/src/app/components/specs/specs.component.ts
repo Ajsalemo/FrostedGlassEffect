0cc90e9cc01e72835d96c9a797d6471140a8bf37
Anthony Salemo <ajssalemo@gmail.com>
2021-01-10 18:38:41 -0500

    adding error handlers for component and services

--- client/src/app/components/specs/specs.component.ts
+++ client/src/app/components/specs/specs.component.ts
@@ -6,10 +6,25 @@ import { GetSpecificationsService } from '@services/getspecifications.service.ts
 })
 export class SpecsComponent implements OnInit {
   carImageFront = 'assets/images/m235i_front.png';
+  errorStatus = '';
+  errorStatusText = '';
+  isError = false;
 
   constructor(private getSpecificationsService: GetSpecificationsService) {}
 
   ngOnInit() {
-    this.getSpecificationsService.getSpecifications();
+    this.isError = false;
+    this.getSpecificationsService.getSpecifications().subscribe(
+      (res: object) => {
+        this.isError = false;
+        console.log(res);
+      },
+      (err: any) => {
+        console.log(err)
+        this.isError = true;
+        this.errorStatus = err.status;
+        this.errorStatusText = err.statusText;
+      }
+    );
   }
 }
--- client/src/app/services/getspecifications.service.ts
+++ client/src/app/services/getspecifications.service.ts
@@ -1,36 +1,15 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { throwError } from 'rxjs/internal/observable/throwError';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class GetSpecificationsService {
+  BASE_API_URL = 'https://frosted-glass-api.azurewebsites.net/specifications';
+
   constructor(private httpClient: HttpClient) {}
 
-  async getSpecifications() {
-    const BASE_API_URL =
-      'https://frosted-glass-api.azurewebsites.net/specifications';
-    try {
-      return this.httpClient
-        .get(BASE_API_URL)
-        .toPromise()
-        .catch((err: any) => {
-          this.error(err);
-        });
-    } catch (err: any) {
-      return this.error(err);
-    }
-  }
-
-  // Handle Errors
-  error(error: HttpErrorResponse) {
-    let errorMessage = '';
-    if (error.error instanceof ErrorEvent) {
-      errorMessage = error.error.message;
-    } else {
-      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
-    }
-    console.log(errorMessage);
-    return throwError(errorMessage);
+  getSpecifications(): Observable<object> {
+    return this.httpClient.get(this.BASE_API_URL);
   }
 }
