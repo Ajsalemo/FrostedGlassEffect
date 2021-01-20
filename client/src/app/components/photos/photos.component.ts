aae70f05fd915b60baf3cf254c75ca90746ea138
Anthony Salemo <ajssalemo@gmail.com>
2021-01-19 20:58:31 -0500

    added a placeholder view to the photos path

--- client/src/app/components/photos/photos.component.ts
+++ client/src/app/components/photos/photos.component.ts
@@ -1,7 +1,28 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
 import { Component } from '@angular/core';
-
+// This component can potentially be refactored into a reusable component with specs.component
 @Component({
   selector: 'app-photos',
   templateUrl: './photos.component.html',
+  animations: [
+    trigger('fadeInOut', [
+      state(
+        'void',
+        style({
+          opacity: 0,
+        })
+      ),
+      transition('void <=> *', animate(1000)),
+    ]),
+  ],
 })
-export class PhotosComponent {}
+export class PhotosComponent {
+  isError = false;
+  isLoading = false;
+}
